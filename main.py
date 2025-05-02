8f42de429388757c2becb8dbb1d5a9e8f4c3ed2f
Juan Manuel Giulietti <juanmagiulietti04@gmail.com>
2025-05-01 22:15:34 -0300

    Agrego la funcion elegirSimbolo()

--- main.py
+++ main.py
@@ -11,11 +11,27 @@ def imprimirTablero():
         print(f"{fila}")
     return tablero
 
+def elegirSimbolo():
+    """
+    La funcion elegirSimbolo() le da al usuario la opcion de elegir con que simbolo quiere jugar y valida su entrada.
+    """
+    simbolo_1 = str(input("Elige el simbolo con el que desea jugar (X o O): ")).upper()
+    while simbolo_1 != "X" and simbolo_1 != "O":
+        print("Por favor ingrese una respuesta valida.")
+        simbolo_1 = str(input("Elige el simbolo con el que desea jugar (X o O): ")).upper()
+        
+    if simbolo_1 == "X":
+        simbolo_2 = "O"
+    else:
+        simbolo_2 = "X"
+        simbolo_1 = "O"
+    return simbolo_1, simbolo_2
+
 def darBienvenidaAlUsuario():
     """
     La funcion darBienvenidaAlUsuario() pide al usuario ingresar su nombre para darle la bienvenida y comenzar el juego.
     """
-    usuario = str(input("Ingrese su nombre para empezar a jugar: "))
+    usuario = str(input("Ingrese su nombre para empezar a jugar: ")).capitalize()
     return usuario
 def main():
     """
@@ -26,4 +42,7 @@ def main():
     print(f"¡Hola! 👋 {usuario} . Bienvenido al juego de Tic - Tac - Toe. Buena suerte 🍀")
     print()
     tablero = imprimirTablero()
+    print()
+    simbolo_1, simbolo_2 = elegirSimbolo()
+    print(f"{usuario} juega con {simbolo_1},\nComputadora juega con {simbolo_2}")
 main()
