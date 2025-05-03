b0ee64542ea9994f7d0d8fc8fca591ef7278f528
Juan Manuel Giulietti <juanmagiulietti04@gmail.com>
2025-05-03 15:50:03 -0300

    Correcion de estructa y logica del programa

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import random
 
-
-
 def indicarTurnos(jugadorComienza, simbolo_1, simbolo_2):
     """
     La funcion indicarTurnos(jugadorComienza, simbolo_1, simbolo_2) define de quien es cada marca, y define cuando sera el turno de cada jugador.
@@ -20,9 +18,9 @@ def elegirQuienComienza(usuario):
     jugadorComienza = random.choice(jugadores)
     return jugadorComienza
 
-def imprimirTablero():
+def crearEImprimirTablero():
     """
-    La funcion crearTablero() crea una matriz con espacios vacios donde el usuario eligira donde insertar su X u O.
+    La funcion crearTablero() crea una matriz con espacios vacios donde el usuario eligira donde insertar su X u O y la imprime.
     """
     tablero = [[" ", " ", " "],
                [" ", " ", " "],
@@ -44,14 +42,16 @@ def elegirSimbolo():
         simbolo_2 = "O"
     else:
         simbolo_2 = "X"
-        simbolo_1 = "O"
     return simbolo_1, simbolo_2
 
 def darBienvenidaAlUsuario():
     """
-    La funcion darBienvenidaAlUsuario() pide al usuario ingresar su nombre para darle la bienvenida y comenzar el juego.
+    La funcion darBienvenidaAlUsuario() pide al usuario ingresar su nombre y lo valida, para darle la bienvenida y comenzar el juego.
     """
     usuario = str(input("Ingrese su nombre para empezar a jugar: ")).capitalize()
+    while usuario == " ":
+        print("Por favor ingrese una respuesta valida: ")
+        usuario = str(input("Ingrese su nombre para empezar a jugar: ")).capitalize()
     return usuario
 def main():
     """
@@ -61,7 +61,7 @@ def main():
     print()
     print(f"¡Hola! 👋 {usuario} . Bienvenido al juego de Tic - Tac - Toe. Buena suerte 🍀")
     print()
-    tablero = imprimirTablero()
+    tablero = crearEImprimirTablero()
     print()
     simbolo_1, simbolo_2 = elegirSimbolo()
     print(f"{usuario} juega con {simbolo_1},\nComputadora juega con {simbolo_2}")
